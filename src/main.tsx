4649843a34f045dd591caca6bf1f934f802c6342
Alessandro Gallo <alessandro.gallo@besharp.it>
2025-08-10 15:15:14 +0200

    feat: added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router';
 import './styles/index.css';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
